24f2edae89c56f6b1b1d04b0ec2f05cc85b9a90c
Matti Korttila <matti.korttila@sysart.fi>
2022-04-04 13:45:22 +0300

    Add browser tests for TET Admin

---
+++ frontend/tet/admin/browser-tests/actions/employer-header.actions.ts
@@ -0,0 +1,18 @@
+import { doLogin, SuomiFiData } from '@frontend/shared/browser-tests/actions/login-action';
+import { getHeaderComponents } from '@frontend/shared/browser-tests/components/header.components';
+import { DEFAULT_LANGUAGE } from '@frontend/shared/src/i18n/i18n';
+import User from '@frontend/shared/src/types/user';
+import TestController from 'testcafe';
+import getEmployerTranslationsApi from '../../src/__tests__/utils/i18n/get-employer-translations-api';
+
+export const doEmployerLogin = async (
+  t: TestController,
+  lang = DEFAULT_LANGUAGE,
+  cachedUser?: User,
+): Promise<SuomiFiData> => {
+  const { translations } = getEmployerTranslationsApi();
+  const headerUser = getHeaderComponents(t, translations).headerUser();
+  await headerUser.expectations.userIsLoggedOut();
+  await headerUser.actions.clickloginButton();
+  return doLogin(t, lang, cachedUser);
+};
